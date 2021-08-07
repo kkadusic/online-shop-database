2948fb510fc7b93e8af3b33a3861452e789807ea
Kerim Kadušić <44180058+kkadusic@users.noreply.github.com>
2021-08-07 12:53:39 +0200

    Update procedure.sql

--- Scripts/procedure.sql
+++ Scripts/procedure.sql
@@ -1,4 +1,4 @@
---Nepotpuno kreiranje procedure 5.1.
+-- Nepotpuno kreiranje procedure 5.1.
 CREATE OR REPLACE PROCEDURE proc_obrisi_starije_fakt
        ( datum  IN Racun.DatumZakljucenja%TYPE )
 IS
@@ -7,3 +7,6 @@ BEGIN
       WHERE  DatumZakljucenja < datum;
       COMMIT;
 END;
+
+-- Delete next line, only for TSQL GitHub language detection
+DECLARE @test VARCHAR(255)
